5c012be8104f8d3f63c29c0ed2b41f60c68f1ecc
Masahiro Sakai <masahiro.sakai@gmail.com>
2016-01-05 11:19:01 +0900

    build windows installer on AppVeyor

---
+++ windows/build_msi.hs
@@ -0,0 +1,34 @@
+{-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+import Control.Exception
+import Control.Monad
+import Data.Version
+import Distribution.Package
+import Distribution.PackageDescription
+import Distribution.PackageDescription.Parse
+import Distribution.Verbosity
+import qualified System.Info as SysInfo
+import System.Process
+
+getGitHash :: IO (Maybe String)
+getGitHash =
+  liftM (Just . takeWhile (/='\n')) (readProcess "git" ["rev-parse", "--short", "HEAD"] "")
+  `catch` \(_::SomeException) -> return Nothing
+
+getVersion :: FilePath -> IO Version
+getVersion cabalFile = do
+  pkg <- readPackageDescription silent cabalFile
+  return $ pkgVersion $ package $ packageDescription $ pkg
+
+main :: IO ()
+main = do
+  gitHashMaybe <- getGitHash
+  version <- getVersion "../CPL.cabal"
+  let suffix_githash =
+        case gitHashMaybe of
+          Nothing -> ""
+          Just gitHash -> "_" ++ gitHash
+      msiFileName = "CPL-" ++ showVersion version ++ suffix_githash ++ "_" ++ SysInfo.arch ++ ".msi"
+  callProcess "candle" ["CPL.wxs"]
+  callProcess "light" ["-ext", "WixUIExtension", "-out", msiFileName, "CPL.wixobj"]
+  return ()
